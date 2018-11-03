ab425ca16dcf2b6e033d7dc083b16cbdf1912e1d
shymaSane <shymaSane1@gmail.com>
2018-11-03 17:57:27 +0200

    project setup with contact component added

---
+++ src/component/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Shyma Sane</h4>
+                <ul>
+                    <li>Email: shyma@gmail.com</li>
+                    <li>Phone Number: 444-444-4444</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Contact;
