ec62e6ccb79ba50eb45e262515dea3b7c3b5318b
shymaSane <shymaSane1@gmail.com>
2018-11-09 17:26:48 +0200

    Deletion button fixed

--- src/Context.js
+++ src/Context.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 
 const Context = React.createContext({})
 
+const reducer = (state, action) =>{
+    switch(action.type){
+        case 'DELETE_CONTACT': 
+            return {
+                ...state,
+                contacts: state.contacts.filter(contact =>  contact.id !== action.payload, console.log(state))
+            };
+        default: 
+            return state;    
+    }
+}
+
 export class Provider extends Component {
     state = {
         contacts:
@@ -24,14 +36,16 @@ export class Provider extends Component {
                 email: 'rahma@gmail.com',
                 phone: '444-444-444'
             }
-        ]
+        ],
+
+        dispatch: action => this.setState(state => reducer(state, action))
     }
 
     render(){
         return(
             <Context.Provider value={this.state}>
                 {this.props.children}
-            </Context.Provider>
+            </Context.Provider> 
         )
     }
 
