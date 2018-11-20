e17476487e6ff0752df4229481fc620235ef06ce
shymaSane <shymaSane1@gmail.com>
2018-11-20 19:22:28 +0200

    setting basic sotre(not working yet)

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import './App.css';
 import Header from './component/Header';
 import './component/Contacts'
 import Contacts from './component/Contacts';
+import {Provider} from 'react-redux';
+import store from './store'
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header title="Contact Manager1"/>
-        <div className = "container">
-            {/* <Contact name="shyma al sane" email="shyma@gmail.com" phone="444-444-4444" />
-            <Contact name="Jumana al sane" email="jumana@gmail.com" phone="342-544-4444" /> */}
-            <Contacts />
+      <Provider store={store}>
+        <div className="App">
+          <Header title="Contact Manager1"/>
+          <div className = "container">
+              {/* <Contact name="shyma al sane" email="shyma@gmail.com" phone="444-444-4444" />
+              <Contact name="Jumana al sane" email="jumana@gmail.com" phone="342-544-4444" /> */}
+              <Contacts />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
